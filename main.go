2e8c55dab45a2ae58881f6c80cba36fb544b92dd
Tad V <tad@etasoft.com>
2019-01-12 14:22:56 -0500

    initial

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	dir := flag.String("dir", "download/", "directory to serve")
+	port := flag.Int("port", 8111, "port to listen on")
+	setNoCache := flag.Bool("no-cache", false, "set no-cache header on requests")
+
+	flag.Parse()
+
+	log.Println("Serving files from", *dir, "on port", *port)
+
+	fserver := http.FileServer(http.Dir(*dir))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RemoteAddr, r.URL)
+		if *setNoCache {
+			w.Header().Set("Cache-Control", "no-cache")
+		}
+
+		fserver.ServeHTTP(w, r)
+	})
+
+	portStr := fmt.Sprintf(":%d", *port)
+	err := http.ListenAndServe(portStr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
